30146c677343825ae6eb6bd80c7a7669bef489bf
lofiandcode <josephtrettevik@gmail.com>
2022-07-12 11:19:13 -0700

    Create GET all to-dos route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const pool = require('./db');
 const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+//CREATE a to-do
+
+//GET all to-dos
+app.get('/todos', async (req, res) => {
+    try {
+        const allTodos = await pool.query(
+            'SELECT * FROM todo'
+        );
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+//GET a to-do
+
+//UPDATE a to-do
+
+//DELETE a to-do
 
 const PORT = 3001;
 
